e700e2296cc50c80d93731239291d0f0d7869469
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-09-20 01:25:46 +1000

    created global error handle function

--- server.js
+++ server.js
@@ -8,17 +8,26 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 
-
-
-
 ///middlewares
 app.use(cors())
 app.use(helmet())
+app.use(express.json())
 
 
 app.get("/", (req,res)=>{
     res.json({
         message: "got it"
+    })
+})
+
+
+app.use((error, res,req,next)=>{
+    console.log(error)
+    const statusCode = error.status ||404
+
+    res.status(statusCode).json({
+        status:"error",
+        message: error.message
     })
 })
 
