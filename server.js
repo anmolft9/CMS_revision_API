c1402f91ebddd4928631fbeb9cfa37a73d9cef11
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-09-25 22:46:05 +1000

    added the router for Admin user

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
-import  'dotenv/config'
-import express from "express"
-import cors from "cors"
-import helmet from "helmet"
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 8000
-
+const PORT = process.env.PORT || 8000;
 
 ///middlewares
-app.use(cors())
-app.use(helmet())
-app.use(express.json())
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "got it",
+  });
+});
 
-app.get("/", (req,res)=>{
-    res.json({
-        message: "got it"
-    })
-})
+//apis
+import adminUserRouter from "./src/routers/adminUserRouter.js";
 
+app.use("/api/v1/admin-user");
 
-app.use((error, res,req,next)=>{
-    console.log(error)
-    const statusCode = error.status ||404
+app.use((error, res, req, next) => {
+  console.log(error);
+  const statusCode = error.status || 404;
 
-    res.status(statusCode).json({
-        status:"error",
-        message: error.message
-    })
-})
+  res.status(statusCode).json({
+    status: "error",
+    message: error.message,
+  });
+});
 
-
-app.listen(PORT, error=>{
-    error ? console.log(error)
-    :
-    console.log(`server running at: http://localhost:${PORT}`)
-})
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`server running at: http://localhost:${PORT}`);
+});
