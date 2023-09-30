d115564c1a7f83aa17cc6cf1ff47efc5edd28532
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-09-30 22:29:37 +1000

    added all the predefined datatypes for the database

--- src/models/adminUser/AdminUserSchema.js
+++ src/models/adminUser/AdminUserSchema.js
@@ -13,33 +13,32 @@ const adminUserSchema = new mongoose.Schema({
   lName: {
     type: String,
     required: true,
-    maxLength: [20, "First name cannot be longer than 20 characters"],
+    maxLength: [20, "Last name cannot be longer than 20 characters"],
   },
   email: {
     type: String,
     unique: true,
     index: 1,
     required: true,
-    maxLength: [20, "First name cannot be longer than 20 characters"],
+    maxLength: [30, "email cannot be longer than 30 characters"],
   },
   password: {
     type: String,
     required: true,
-    maxLength: [20, "First name cannot be longer than 20 characters"],
+    maxLength: [30, "password cannot be longer than 30 characters"],
   },
   phone: {
     type: String,
     required: true,
-    maxLength: [20, "First name cannot be longer than 20 characters"],
+    maxLength: [30, "phone number cannot be longer than 30 characters"],
   },
   address: {
     type: String,
     required: true,
-    maxLength: [20, "First name cannot be longer than 20 characters"],
+    maxLength: [50, "Address cannot be longer than 50 characters"],
   },
   DOB: {
-    type: String,
+    type: Date,
     required: true,
-    maxLength: [20, "First name cannot be longer than 20 characters"],
   },
 });
