4a13bff91af49e9ec5b615133f4b62c6ac2cc12d
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-10-02 23:46:15 +1100

    added all the predefined datatypes for the database

--- src/models/adminUser/AdminUserSchema.js
+++ src/models/adminUser/AdminUserSchema.js
@@ -1,44 +1,48 @@
 import mongoose from "mongoose";
 
-const adminUserSchema = new mongoose.Schema({
-  status: {
-    type: String,
-    default: inactive,
+const adminUserSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      default: inactive,
+    },
+    fName: {
+      type: String,
+      required: true,
+      maxLength: [20, "First name cannot be longer than 20 characters"],
+    },
+    lName: {
+      type: String,
+      required: true,
+      maxLength: [20, "Last name cannot be longer than 20 characters"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      index: 1,
+      required: true,
+      maxLength: [50, "email cannot be longer than 30 characters"],
+    },
+    password: {
+      type: String,
+      required: true,
+      maxLength: [30, "password cannot be longer than 30 characters"],
+    },
+    phone: {
+      type: String,
+      maxLength: [30, "phone number cannot be longer than 30 characters"],
+    },
+    address: {
+      type: String,
+      maxLength: [50, "Address cannot be longer than 50 characters"],
+      default: "n/a",
+    },
+    DOB: {
+      type: Date,
+      default: null,
+    },
   },
-  fName: {
-    type: String,
-    required: true,
-    maxLength: [20, "First name cannot be longer than 20 characters"],
-  },
-  lName: {
-    type: String,
-    required: true,
-    maxLength: [20, "Last name cannot be longer than 20 characters"],
-  },
-  email: {
-    type: String,
-    unique: true,
-    index: 1,
-    required: true,
-    maxLength: [50, "email cannot be longer than 30 characters"],
-  },
-  password: {
-    type: String,
-    required: true,
-    maxLength: [30, "password cannot be longer than 30 characters"],
-  },
-  phone: {
-    type: String,
-    required: true,
-    maxLength: [30, "phone number cannot be longer than 30 characters"],
-  },
-  address: {
-    type: String,
-    required: true,
-    maxLength: [50, "Address cannot be longer than 50 characters"],
-  },
-  DOB: {
-    type: Date,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
