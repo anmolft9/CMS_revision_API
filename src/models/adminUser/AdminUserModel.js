2af79238204d7c771028ebc7728f7dce01161dac
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-10-08 23:58:53 +1100

    imported the insert user function on the router

--- src/models/adminUser/AdminUserModel.js
+++ src/models/adminUser/AdminUserModel.js
@@ -3,3 +3,7 @@ import AdminUserSchema from "./AdminUserSchema.js";
 export const insertAdminUser = (obj) => {
   return AdminUserSchema(obj).save();
 };
+
+export const insertAdminUser = (obj) => {
+  return AdminUserSchema(obj).save();
+};
