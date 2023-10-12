eb448511d0cb65dd4b8d483de90e673645347d30
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-10-12 22:56:49 +1100

    deleted userModel function as it was not necessary

--- src/models/adminUser/AdminUserModel.js
+++ src/models/adminUser/AdminUserModel.js
@@ -3,10 +3,3 @@ import AdminUserSchema from "./AdminUserSchema.js";
 export const insertAdminUser = (obj) => {
   return AdminUserSchema(obj).save();
 };
-
-export const getAdminUser = (obj) => {
-  return AdminUserSchema(obj).save();
-};
-export const insertAdminUser = (obj) => {
-  return AdminUserSchema(obj).save();
-};
