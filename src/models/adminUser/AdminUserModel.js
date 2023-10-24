07607aba41d703272250e5d60f076cef252fdc81
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-10-24 20:43:08 +0545

    deleted the function that replicated the insertAdminUser function

--- src/models/adminUser/AdminUserModel.js
+++ src/models/adminUser/AdminUserModel.js
@@ -3,6 +3,3 @@ import AdminUserSchema from "./AdminUserSchema.js";
 export const insertAdminUser = (obj) => {
   return AdminUserSchema(obj).save();
 };
-export const insertAdminUser = (obj) => {
-  return AdminUserSchema(obj).save();
-};
