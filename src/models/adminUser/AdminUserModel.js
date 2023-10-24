80ac807c7731fe8ee2a88ea9df0ee994d5054826
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-10-24 20:42:05 +0545

    added a function on admin user model to insert the admin user registration details

--- src/models/adminUser/AdminUserModel.js
+++ src/models/adminUser/AdminUserModel.js
@@ -3,3 +3,6 @@ import AdminUserSchema from "./AdminUserSchema.js";
 export const insertAdminUser = (obj) => {
   return AdminUserSchema(obj).save();
 };
+export const insertAdminUser = (obj) => {
+  return AdminUserSchema(obj).save();
+};
