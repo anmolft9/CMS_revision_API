cc95d269a5d41a18054066495078a75d9fcb4ebc
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-12-19 19:33:45 +0545

    added joi package for data validation

---
+++ src/middlewares/joi-validation/adminUserValidation.js
@@ -0,0 +1,9 @@
+import joi from "joi";
+
+const newAdminUserValidation = (req, res, next) => {
+  try {
+    ///define rules and give data to the rules
+  } catch (error) {
+    next(error);
+  }
+};
