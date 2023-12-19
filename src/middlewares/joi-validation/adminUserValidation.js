e97a3713973179911c90adb2b61d4192d348ba0c
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-12-19 19:34:28 +0545

    created a function to validate user registration data

--- src/middlewares/joi-validation/adminUserValidation.js
+++ src/middlewares/joi-validation/adminUserValidation.js
@@ -3,6 +3,15 @@ import joi from "joi";
 const newAdminUserValidation = (req, res, next) => {
   try {
     ///define rules and give data to the rules
+    const schema = Joi.object({
+      fName,
+      Lname,
+      email,
+      password,
+      phone,
+      address,
+      dob,
+    });
   } catch (error) {
     next(error);
   }
