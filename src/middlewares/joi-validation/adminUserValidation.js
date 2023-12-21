8ee54611e1c8585e9473eda2035a99bec96900c3
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-12-21 18:32:53 +0545

    passed the value from req.body to validate and handled the error

--- src/middlewares/joi-validation/adminUserValidation.js
+++ src/middlewares/joi-validation/adminUserValidation.js
@@ -1,17 +1,24 @@
-import joi from "joi";
+import Joi from "joi";
 
 const newAdminUserValidation = (req, res, next) => {
   try {
     ///define rules and give data to the rules
     const schema = Joi.object({
-      fName,
-      Lname,
-      email,
-      password,
-      phone,
-      address,
-      dob,
+      fName: Joi.string().max(20).required,
+      Lname: Joi.string().max(20).required,
+      email: Joi.string().email({ minDomainSegments: 2 }),
+      password: Joi.string().max(100).required,
+      phone: Joi.string().max(20).required,
+      address: Joi.string().max(200),
+      dob: Joi.date(),
     });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      error.status = 200;
+      return next(error);
+    }
   } catch (error) {
     next(error);
   }
