15caed2948d2d49241c91b5bd2b17cf6df24f8d3
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-12-25 19:19:39 +0545

    added the info to send to the end user/admin for verification

--- src/helpers/emailHelper.js
+++ src/helpers/emailHelper.js
@@ -4,18 +4,26 @@ import nodemailer from "nodemailer";
 
 //email template
 
-const verificationEmail = (emailData) => {
+const verificationEmail = async (emailData) => {
   try {
     //1.
     const transporter = nodemailer.createTransport({
-      host: "smtp.forwardemail.net",
-      port: 465,
+      host: process.env.EMAIL_SMTP,
+      port: process.env.EMAIL_PORT,
       secure: false,
       auth: {
         // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-        user: "REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM",
-        pass: "REPLACE-WITH-YOUR-GENERATED-PASSWORD",
+        user: EMAIL_PORT.EMAIL_USER,
+        pass: EMAIL_PORT.EMAIL_PASSWORD,
       },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: "bar@example.com, baz@example.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
     });
   } catch (error) {
     console.log(error);
