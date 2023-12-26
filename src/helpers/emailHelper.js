06469e2cb6dad3b87c6b8c7bbf65a97ea12f3c7c
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-12-26 19:24:58 +0545

    made a separate verification email template for reuse>

--- src/helpers/emailHelper.js
+++ src/helpers/emailHelper.js
@@ -4,28 +4,33 @@ import nodemailer from "nodemailer";
 
 //email template
 
-const verificationEmail = async (emailData) => {
+const emailProcessor = async (emailBody) => {
   try {
     //1.
-    const transporter = nodemailer.createTransport({
+    let transporter = nodemailer.createTransport({
       host: process.env.EMAIL_SMTP,
       port: process.env.EMAIL_PORT,
       secure: false,
       auth: {
         // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-        user: EMAIL_PORT.EMAIL_USER,
-        pass: EMAIL_PORT.EMAIL_PASSWORD,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
 
-    const info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
+    const info = await transporter.sendMail(emailBody);
   } catch (error) {
     console.log(error);
   }
+};
+
+const verificationEmail = (maileData) => {
+  const emailBody = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  };
+  emailProcessor(emailBody);
 };
