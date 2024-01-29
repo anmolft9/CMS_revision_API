d16e5a0dd2b7980b770a042090c58a0e5288303c
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-01-29 20:30:21 +0545

    added constant.js file which holds the variables for the validation

---
+++ src/middlewares/joi-validation/constant.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const FNAME = Joi.string().max(20);
+export const LNAME = Joi.string().max(20);
+export const EMAIL = Joi.string().email({ minDomainSegments: 2 });
+export const PASSWORD = Joi.string().max(100);
+export const PHONE = Joi.string().max(20);
+export const ADDRESS = Joi.string().max(200).allow("", null);
+export const DATE = Joi.date();
+
+export const SHORTSTR = Joi.string().max(50);
+export const LONGSTR = Joi.string().max(5000);
