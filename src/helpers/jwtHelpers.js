c3dd7e79a9ce67c676ba2ee48bc88444ceb2a7d8
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-02-13 22:42:05 +0545

    added the jwt function to add the user data as a token

--- src/helpers/jwtHelpers.js
+++ src/helpers/jwtHelpers.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 
+///create token using the user data
 export const signAccessJWT = (payload) => {
   return jwt.sign({ payload }, process.env.JWT_ACCESS_SECRET);
 };
