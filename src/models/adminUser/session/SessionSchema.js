e0f5f9b639b0e60635487f41b3a51b34aa25b3bb
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-02-15 20:12:14 +0545

    created sessionSchema to hold the temporary data on the database

---
+++ src/models/adminUser/session/SessionSchema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    token: {
+      type: "string",
+      required: true,
+    },
+    associate: {
+      type: "string",
+      default: "",
+    },
+    type: {
+      type: "string",
+      required: true,
+    },
+    expires: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default sessionSchema;
