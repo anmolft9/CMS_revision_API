95617dadfa9c21cc8e3a9763fe9b01723261519f
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-02-28 18:31:59 +0545

    added a new package called slugify and added the slug to the database

--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { insertCategory } from "../models/category/CategoryModel.js";
 import { newCategoryValidation } from "../middlewares/joi-validation/joiValidation.js";
+import slugify from "slugify";
 
 const router = express.Router();
 
 router.post("/", newCategoryValidation, async (req, res, next) => {
   try {
+    // console.log(req.body);
+    req.body.slug = slugify(req.body.name, {
+      lower: true,
+      trim: true,
+    });
     const result = await insertCategory(req.body);
+
     result?._id
       ? res.json({
           status: "success",
