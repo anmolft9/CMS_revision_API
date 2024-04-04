84d259980a6718067dac03c03c8644ada55d873c
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-04-04 22:18:43 +1100

    added timestamps to category schema

--- src/models/category/CategorySchema.js
+++ src/models/category/CategorySchema.js
@@ -1,27 +1,32 @@
 import mongoose from "mongoose";
 
-const CategorySchema = new mongoose.Schema({
-  status: {
-    type: String,
-    default: "inactive",
+const CategorySchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      default: "inactive",
+    },
+    name: {
+      type: String,
+      required: true,
+      maxLength: 50,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      index: 1,
+      maxLength: 50,
+      trim: true,
+    },
+    parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: null,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-    maxLength: 50,
-  },
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-    index: 1,
-    maxLength: 50,
-    trim: true,
-  },
-  parentId: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: null,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("category", CategorySchema);
